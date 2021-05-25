91609dd6387d0153c53bffbfb0450752af215675
codyjamesbrooks <71521423+codyjamesbrooks@users.noreply.github.com>
2021-05-25 13:45:56 -0600

    correct tests on valid puzzle, and invalid puzzles

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -2,30 +2,30 @@ const chai = require("chai");
 const assert = chai.assert;
 
 const Solver = require("../controllers/sudoku-solver.js");
-const puzzleAndSolutions = require("../controllers/puzzle-strings.js");
+const puzzles = require("../controllers/puzzle-strings").puzzlesAndSolutions;
 let solver = new Solver();
 
 const invalidCharacters =
   "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()/-[]{} :\"',0";
-let invalidPuzzle;
+let invalidPuzzle, randomSliceIndex, randomInvalidChar;
 
 suite("UnitTests", () => {
   suite("testing Solver validate method", function () {
     test("correctly validates valid puzzles", () => {
-      for (let i = 0; i < puzzleAndSolutions.length; i++) {
+      for (let i = 0; i < puzzles.length; i++) {
         assert.deepEqual(
-          solver.validate(puzzleAndSolutions[i][0]),
+          solver.validate(puzzles[i][0]),
           {
             valid: true,
           },
-          `validate method output: ${solver.validate(puzzleAndSolutions[i][0])}`
+          `validate method output: ${solver.validate(puzzles[i][0])}`
         );
       }
     });
 
     test("returns error msg when puzzle string has invalid characters", () => {
-      for (let i = 0; i < puzzleAndSolutions.length; i++) {
-        invalidPuzzle = puzzleAndSolutions[i][0].slice();
+      for (let i = 0; i < puzzles.length; i++) {
+        invalidPuzzle = puzzles[i][0].slice();
         randomSliceIndex = Math.floor(Math.random() * 81);
         randomInvalidChar = invalidCharacters.charAt(
           Math.floor(Math.random() * 75)
@@ -44,16 +44,32 @@ suite("UnitTests", () => {
         );
       }
     });
+    let invalidShort = puzzles[0][0].slice(1);
+    let invalidLong = invalidShort + "..";
+    test("returns error msg when puzzle string has incorrect number of chars", () => {
+      assert.deepEqual(
+        solver.validate(invalidShort),
+        {
+          error: "Expected puzzle to be 81 characters long",
+        },
+        "Failed to identify short puzzle"
+      );
+      assert.deepEqual(
+        solver.validate(invalidLong),
+        {
+          error: "Expected puzzle to be 81 characters long",
+        },
+        "Failed to identify long puzzle"
+      );
+    });
   });
-
-  // Logic handles a puzzle string that is not 81 characters in length
-  // Logic handles a valid row placement
-  // Logic handles an invalid row placement
-  // Logic handles a valid column placement
-  // Logic handles an invalid column placement
-  // Logic handles a valid region (3x3 grid) placement
-  // Logic handles an invalid region (3x3 grid) placement
-  // Valid puzzle strings pass the solver
-  // Invalid puzzle strings fail the solver
-  // Solver returns the the expected solution for an incomplete puzzle
 });
+// Logic handles a valid row placement
+// Logic handles an invalid row placement
+// Logic handles a valid column placement
+// Logic handles an invalid column placement
+// Logic handles a valid region (3x3 grid) placement
+// Logic handles an invalid region (3x3 grid) placement
+// Valid puzzle strings pass the solver
+// Invalid puzzle strings fail the solver
+// Solver returns the the expected solution for an incomplete puzzle
